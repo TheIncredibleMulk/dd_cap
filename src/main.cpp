68ca523e2ec0703a98f88bc08afb98f1900b9779
TheIncredibleMulk <andrew.mulkey@gmail.com>
2021-03-07 14:34:06 -0500

    adding

--- src/main.cpp
+++ src/main.cpp
@@ -3,13 +3,12 @@
 /*
  * CapitiveSense Library Demo Sketch
  * Paul Badger 2008
- * Adding updates for art installation project.
+ * Adding updates for art installation project DeeDee Morrison Studios
  * Andrew Mulkey 2021
  * Uses a high value resistor e.g. 10M between send pin and receive pin
  * Resistor effects sensitivity, experiment with values, 50K - 50M. Larger resistor values yield larger sensor values.
  * Receive pin is the sensor pin - try different amounts of foil/metal on this pin
  */
-
 
 // set send and recieve pin numbers
 int send1 = 2;
@@ -20,16 +19,18 @@ int send3 = 6;
 int recieve3 = 7;
 int send4 = 8;
 int recieve4 = 9;
-int relay = 12;                 // set GPIO Relay/Opto Coupler value
-long previousMillis = 0;        //start the counter so we can count and keep sampling capacitive touch
-long interval = 1000;           //amount of time you want to wait until you can send a closure again
+int relay = 12; // set GPIO Relay/Opto Coupler value
 
-CapacitiveSensor   cs_send1_recieve1 = CapacitiveSensor(send1,recieve1);        // 1M resistor between pins 6 & 3, pin 3 is sensor pin, add a wire and or foil
-CapacitiveSensor   cs_send2_recieve2 = CapacitiveSensor(send2,recieve2);        // 1M resistor between pins 7 & 4, pin 5 is sensor pin, add a wire and or foil
-CapacitiveSensor   cs_send3_recieve3 = CapacitiveSensor(send3,recieve3);        // 1M resistor between pins 8 & 5, pin 5 is sensor pin, add a wire and or foil
-CapacitiveSensor   cs_send4_recieve4 = CapacitiveSensor(send4,recieve4);        // 1M resistor between pins 11 & 9, pin 5 is sensor pin, add a wire and or foil
+long capPreviousMillis = 0; //start the counter so we can count and keep sampling capacitive touch
+long capInterval = 1000;    //amount of time you want to wait until you can send a closure again
 
-void setup()  {
+CapacitiveSensor cs_send1_recieve1 = CapacitiveSensor(send1, recieve1); // 1M resistor between pins 6 & 3, pin 3 is sensor pin, add a wire and or foil
+CapacitiveSensor cs_send2_recieve2 = CapacitiveSensor(send2, recieve2); // 1M resistor between pins 7 & 4, pin 5 is sensor pin, add a wire and or foil
+CapacitiveSensor cs_send3_recieve3 = CapacitiveSensor(send3, recieve3); // 1M resistor between pins 8 & 5, pin 5 is sensor pin, add a wire and or foil
+CapacitiveSensor cs_send4_recieve4 = CapacitiveSensor(send4, recieve4); // 1M resistor between pins 11 & 9, pin 5 is sensor pin, add a wire and or foil
+
+void setup()
+{
   //cs_send1_recieve1.set_CS_AutocaL_Millis(0xFFFFFFFF);     // turn off autocalibrate on channel 1
   //cs_send2_recieve2.set_CS_AutocaL_Millis(0xFFFFFFFF);     // turn off autocalibrate on channel 2
   //cs_send3_recieve3.set_CS_AutocaL_Millis(0xFFFFFFFF);     // turn off autocalibrate on channel 3
@@ -40,39 +41,48 @@ void setup()  {
   cs_send4_recieve4.set_CS_Timeout_Millis(100000);
 
   Serial.begin(9600);
-  pinMode(LED_BUILTIN, OUTPUT);                         //set d13 as led output for sensing readout
+  pinMode(LED_BUILTIN, OUTPUT); //set d13 as led output for sensing readout
   pinMode(relay, OUTPUT);
-  }
+}
 
-
-
-void loop()                    
+void loop()
 {
-    long start = millis();
-      long total1 =  cs_send1_recieve1.capacitiveSensor(30);
-      long total2 =  cs_send2_recieve2.capacitiveSensor(30);
-      long total3 =  cs_send3_recieve3.capacitiveSensor(30);
-      long total4 =  cs_send4_recieve4.capacitiveSensor(30);
+  long start = millis();
+  long total1 = cs_send1_recieve1.capacitiveSensor(30);
+  long total2 = cs_send2_recieve2.capacitiveSensor(30);
+  long total3 = cs_send3_recieve3.capacitiveSensor(30);
+  long total4 = cs_send4_recieve4.capacitiveSensor(30);
+  long capCurrentMillis = millis();
+  delay(1000);
+  Serial.println();
+  Serial.print(millis() - start); // check on performance in milliseconds
+  Serial.print("\t");             // tab character for debug windown spacing
+  Serial.print(total1);           // print sensor output 1
+  Serial.print("\t");
+  Serial.print(total2); // print sensor output 2
+  Serial.print("\t");
+  Serial.print(total3); // print sensor output 3
+  Serial.print("\t");
+  Serial.println(total4); // print sensor output 4
+  Serial.print("\t");
+  Serial.println();
+  Serial.print(capCurrentMillis); // print CapCurrentMillis
+  Serial.print(" - ");
+  Serial.print(capPreviousMillis); // print capPreviousMillis
+  Serial.print(" > ");
+  Serial.print(capInterval); // print capInterval
+  Serial.println();
+  // capPreviousMillis - capCurrentMillis = capIntervalDebug;
 
-  Serial.print(millis() - start);        // check on performance in milliseconds
-  Serial.print("\t");                    // tab character for debug windown spacing
-  Serial.print(total1);                // print sensor output 1
-  Serial.print("\t");
-  Serial.print(total2);                // print sensor output 2
-  Serial.print("\t");
-  Serial.print(total3);                // print sensor output 3
-  Serial.print("\t");
-  Serial.println(total4);                // print sensor output 4
-  
+  Serial.print(capCurrentMillis)
 
-  bool relayState = false;              // set true/false value for how the relay should switch. 
-  
-  unsigned long currentMillis = millis(); 
-  if (currentMillis - previousMillis > interval )
+      bool relayState = false; // set true/false value for how the relay should switch.
+
+  if (capCurrentMillis - capPreviousMillis > capInterval)
   {
-   previousMillis = currentMillis;
+    capPreviousMillis = capCurrentMillis;
 
-    if (total1 >= 500 or total2 >= 500 or total3 >= 500 or total4 >= 500)                   // look to see if the capacitive sensing value is high enough to trigger relay
+    if (total1 >= 500 or total2 >= 500 or total3 >= 500 or total4 >= 500) // look to see if the capacitive sensing value is high enough to trigger relay
     {
       digitalWrite(LED_BUILTIN, HIGH);
       digitalWrite(relay, HIGH);
@@ -81,13 +91,12 @@ void loop()
     else
     {
       digitalWrite(LED_BUILTIN, LOW);
-      digitalWrite(relay,LOW);
+      digitalWrite(relay, LOW);
       relayState = false;
     }
-  }    
+  }
 
-
-/*
+  /*
   void switchFunction (bool status) {
      if (status = true)
      {
@@ -99,20 +108,11 @@ void loop()
     }
 */
 
+  // TESTING AREA
 
-  
-    // TESTING AREA 
-  
-    
-    
-
-
-    /* if (total3 > 20) digitalWrite(LED_BUILTIN, HIGH);
+  /* if (total3 > 20) digitalWrite(LED_BUILTIN, HIGH);
     else delay(1000); 
     digitalWrite(LED_BUILTIN, LOW); */
 
-
-    // need to set output delay so touch is instant but off is delayed
-    
-
-  }
+  // need to set output delay so touch is instant but off is delayed
+}
