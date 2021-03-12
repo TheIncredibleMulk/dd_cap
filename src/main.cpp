c2a86655a7ac85579a7d998789948fb2ba2454a5
TheIncredibleMulk <andrew.mulkey@gmail.com>
2021-03-12 16:23:54 -0500

    new trigger threshold and finalizing

--- src/main.cpp
+++ src/main.cpp
@@ -20,10 +20,11 @@ int recieve3 = 7;
 int send4 = 8;
 int recieve4 = 9;
 int relay = 12; // set GPIO Relay/Opto Coupler value
-int triggerThreshold = 3500;
+int triggerThreshold = 200;
 
 long capPreviousMillis = 0; //start the counter so we can count and keep sampling capacitive touch
 long capInterval = 500;     //amount of time you want to wait until you can send a closure again
+bool relayState = false;    // set true/false value for how the relay should switch.
 
 CapacitiveSensor cs_send1_recieve1 = CapacitiveSensor(send1, recieve1); // 1M resistor between pins 6 & 3, pin 3 is sensor pin, add a wire and or foil
 CapacitiveSensor cs_send2_recieve2 = CapacitiveSensor(send2, recieve2); // 1M resistor between pins 7 & 4, pin 5 is sensor pin, add a wire and or foil
@@ -55,26 +56,22 @@ void loop()
   long total4 = cs_send4_recieve4.capacitiveSensor(30);
   long capCurrentMillis = millis();
 
-  if (capCurrentMillis - capPreviousMillis > capInterval)
-  {
-    //delay(250);
-    Serial.println();
-    Serial.print(millis() - start); // check on performance in milliseconds
-    Serial.print("\t");             // tab character for debug windown spacing
-    Serial.print(total1);           // print sensor output 1
-    Serial.print("\t");
-    Serial.print(total2); // print sensor output 2
-    Serial.print("\t");
-    Serial.print(total3); // print sensor output 3
-    Serial.print("\t");
-    Serial.println(total4); // print sensor output 4
-    Serial.print("\t");
-  }
-  bool relayState = false; // set true/false value for how the relay should switch.
+  Serial.println();
+  Serial.print(millis() - start); // check on performance in milliseconds
+  Serial.print("\t");             // tab character for debug windown spacing
+  Serial.print(total1);           // print sensor output 1
+  Serial.print("\t");
+  Serial.print(total2); // print sensor output 2
+  Serial.print("\t");
+  Serial.print(total3); // print sensor output 3
+  Serial.print("\t");
+  Serial.print(total4); // print sensor output 4
+  Serial.print("\t");
+  Serial.print(capCurrentMillis - capPreviousMillis); // print sensor output 4
+  Serial.print("\t");
 
   if (capCurrentMillis - capPreviousMillis > capInterval)
   {
-
     if (total1 >= triggerThreshold or total2 >= triggerThreshold or total3 >= triggerThreshold or total4 >= triggerThreshold) // look to see if the capacitive sensing value is high enough to trigger relay
     {
       capPreviousMillis = capCurrentMillis;
